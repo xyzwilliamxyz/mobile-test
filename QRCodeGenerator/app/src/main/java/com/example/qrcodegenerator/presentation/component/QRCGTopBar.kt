bb419dd5221ac4a339dd7f7505e6eda606c1d2cf
william <wiltang256@gmail.com>
2025-05-14 17:26:23 -0300

    Add top bar component

---
+++ QRCodeGenerator/app/src/main/java/com/example/qrcodegenerator/presentation/component/QRCGTopBar.kt
@@ -0,0 +1,61 @@
+package com.example.qrcodegenerator.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.qrcodegenerator.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QRCGTopBar(
+    title: String,
+    actions: @Composable RowScope.() -> Unit = {},
+    onBackClick: (() -> Unit)? = null,
+) {
+    TopAppBar(
+        navigationIcon = {
+            if (onBackClick != null) {
+                IconButton(onClick = onBackClick) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = stringResource(R.string.back_content_description))
+                }
+            }
+        },
+        title = { Text(title) },
+        actions = actions,
+        colors = TopAppBarDefaults.topAppBarColors(
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+    )
+}
+
+@Preview
+@Composable
+private fun QRCGTopBar_Preview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        QRCGTopBar(
+            "Title",
+            onBackClick = {},
+        )
+    }
+}
