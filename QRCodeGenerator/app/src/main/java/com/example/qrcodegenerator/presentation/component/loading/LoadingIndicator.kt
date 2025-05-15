0fc80ceae006140b138d23d38fcfd1a84bf150cf
william <wiltang256@gmail.com>
2025-05-14 22:01:39 -0300

    Add loading indicator

---
+++ QRCodeGenerator/app/src/main/java/com/example/qrcodegenerator/presentation/component/loading/LoadingIndicator.kt
@@ -0,0 +1,20 @@
+package com.example.qrcodegenerator.presentation.component.loading
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun LoadingIndicator(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        CircularProgressIndicator()
+    }
+}
